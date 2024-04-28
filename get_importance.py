bd4123c5c53ca7d2ca6968e11ab2d660cd5f3897
octoclonius <25781800+octoclonius@users.noreply.github.com>
2024-04-28 13:35:36 -0700

    feature importance

---
+++ get_importance.py
@@ -0,0 +1,11 @@
+import joblib
+import json
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+
+model = joblib.load('models/Random_Forest_Classifier_model.pkl')
+with open('columns.json', 'r') as cols:
+    feature_importances = [(feature, importance) for feature, importance in zip(json.load(cols)[1:], model.feature_importances_)]
+feature_importances.sort(key=lambda x: (-x[1], x[0]))
+with open('feature_importances.json', 'w') as file:
+    json.dump(feature_importances, file)
